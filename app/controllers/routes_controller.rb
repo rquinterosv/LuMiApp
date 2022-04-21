1ae30f8323875b84782f95a17f93e884082cbac6
rquinterosv <rafael.quinterosv@gmail.com>
2022-04-21 19:07:41 -0400

    Funciona la app faltan detalles del logiin sin google

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,30 @@
 class RoutesController < InheritedResources::Base
 
+  def index 
+    @route = Route.new
+    @routes = Route.all
+  end
+
+  def create
+    @route = Route.new(route_params)
+    puts @route
+    puts @route.name
+    respond_to do |format|
+      if @route.save
+        @route_new = Route.new
+        format.html { redirect_to route_url(@route), notice: "Route was successfully created." }
+        format.json { render :show, status: :created, location: @route }
+        format.js
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @route.errors, status: :unprocessable_entity }
+        format.js
+        format.turbo_stream
+      end
+    end
+  end
+
   private
 
     def route_params
