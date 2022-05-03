f4812accef853fa570c44826015efd144061627c
rquinterosv <rafael.quinterosv@gmail.com>
2022-05-03 19:59:08 -0400

    se arreglo navbar mobile

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log('Holaaaa')
+    $(".navbar-burger").click(function() {
+      console.log('Chao')
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          $(".navbar-burger").toggleClass("is-active");
+          $(".navbar-menu").toggleClass("is-active");
+    
+      });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
-  resources :routes
+  get '/user', to: redirect('/routes')
+  resources :routes 
   resources :regions
   resources :countries
   root 'routes#index'
