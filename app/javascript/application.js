3639352d4af5dd49e9b0b85c74bcc361acbf9eea
rquinterosv <rafael.quinterosv@gmail.com>
2022-05-03 16:27:50 -0400

    se arregla el dropdown y se inserta imagen de fondo que hay que ajustar a tamaño grande y sin duplicado

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+  
+    });
+  });
